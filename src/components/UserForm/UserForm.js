79afbef29fd667be05cfeae34e77b9471206ca09
Nishant Gurung <nishant.gurung@whitbread.com>
2020-12-31 15:45:42 +0000

    Basic form components

--- src/components/UserForm/UserForm.js
+++ src/components/UserForm/UserForm.js
@@ -1,6 +1,45 @@
+import { Field, Form, Formik } from "formik";
+
 const UserForm = () => {
+  const initialValues = {
+    name: '',
+    role: '',
+    email: '',
+    password: '',
+  };
+
   return (
-    <div> Name: </div>
+    <Formik
+      initialValues={initialValues}
+
+      onSubmit={(values) => {
+        alert(JSON.stringify(values, null, 2));
+      }}
+    >
+
+      <Form>
+        <label htmlFor="name">First Name</label>
+        <Field name="name" placeholder="Ash" />
+
+        <label htmlFor="role">Role</label>
+        <Field name="role" placeholder="Software engineer" />
+
+        <label htmlFor="email">Email</label>
+        <Field
+          name="email"
+          placeholder="ash@ketchum.com"
+          type="email"
+        />
+
+        <label htmlFor="password">Password</label>
+        <Field
+          name="password"
+          placeholder="ash@ketchum.com"
+          type="password"
+        />
+        <button type="submit">Submit</button>
+      </Form>
+    </Formik>
   )
 };
 
