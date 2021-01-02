5f762014f5ba392c4b887b0b5fe4666756d97bdd
Nishant Gurung <nishant.gurung@whitbread.com>
2021-01-02 16:42:19 +0000

    Adding react-router-dom

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import UserForm from '../UserForm';
+import PrivacyForm from '../PrivacyForm';
+import Confirmation from '../Confirmation';
 
 const App = () => {
   return (
-    <div className="App">
-      <UserForm />
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <UserForm />
+        </Route>
+        <Route path="/privacy">
+          <PrivacyForm />
+        </Route>
+        <Route path="/confirmation">
+          <Confirmation />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
