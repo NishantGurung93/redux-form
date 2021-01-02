ff2858d987ec652e2c182905296e522239faac3a
Nishant Gurung <nishant.gurung@whitbread.com>
2021-01-02 21:00:04 +0000

    Confirmation component

--- src/components/Confirmation/Confirmation.js
+++ src/components/Confirmation/Confirmation.js
@@ -1,6 +1,15 @@
+import { useSelector } from "react-redux";
+import './confirmation.css';
+
 const Confirmation = () => {
+  const reduxState = useSelector(state => state);
+  console.log(JSON.stringify(reduxState));
+
   return (
-    <div> Confirmation </div>
+    <>
+      <div className="confirm-checkmark">&#10003;</div>
+      <div className="confirm-message">Please verify your email address, you should have received an email from us already!</div>
+    </>
   )
 };
 
