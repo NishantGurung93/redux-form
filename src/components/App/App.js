47c0fe758b308ee0b0f674299926f07370ccc90f
Nishant Gurung <nishant.gurung@whitbread.com>
2021-01-02 20:58:18 +0000

    Style changes for app

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,24 +4,27 @@ import UserForm from '../UserForm';
 import PrivacyForm from '../PrivacyForm';
 import Confirmation from '../Confirmation';
 import Header from '../Header/Header';
+import './app.css';
 
 const App = () => {
   return (
     <Router>
       <Header />
       <Switch>
-        <Route exact path="/">
-          <Redirect to="/details" />
-        </Route>
-        <Route exact path="/details">
-          <UserForm />
-        </Route>
-        <Route path="/privacy">
-          <PrivacyForm />
-        </Route>
-        <Route path="/confirmation">
-          <Confirmation />
-        </Route>
+        <div className="app">
+          <Route exact path="/">
+            <Redirect to="/details" />
+          </Route>
+          <Route exact path="/details">
+            <UserForm />
+          </Route>
+          <Route path="/privacy">
+            <PrivacyForm />
+          </Route>
+          <Route path="/confirmation">
+            <Confirmation />
+          </Route>
+        </div>
       </Switch>
     </Router>
   );
