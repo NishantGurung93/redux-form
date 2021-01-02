62eb3b5f5ddf8e01bffc2444ea7f56741e87fddf
Nishant Gurung <nishant.gurung@whitbread.com>
2021-01-02 17:21:10 +0000

    Rendering Header component in App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
 import UserForm from '../UserForm';
 import PrivacyForm from '../PrivacyForm';
 import Confirmation from '../Confirmation';
+import Header from '../Header/Header';
 
 const App = () => {
   return (
     <Router>
+      <Header />
       <Switch>
         <Route exact path="/">
+          <Redirect to="/details" />
+        </Route>
+        <Route exact path="/details">
           <UserForm />
         </Route>
         <Route path="/privacy">
